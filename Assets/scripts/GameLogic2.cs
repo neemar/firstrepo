0e97b5d396dc6b1ea877f2130c09b25602ed39ad
Rameen Shafiee <rs4047@nyu.edu>
2015-09-22 13:37:08 -0400

    treasureupdate3

--- Assets/scripts/GameLogic2.cs
+++ Assets/scripts/GameLogic2.cs
@@ -2,43 +2,30 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class GameLogic : MonoBehaviour {
+public class GameLogic2 : MonoBehaviour {
 	public Text textUI;
 	public Transform player;
 	public Transform goal;
-	bool hasFoundTreasure = false;
 
 	// Use this for initialization
 	void Start () {
-
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//textBuffer += "\npress [W] to go forward";
-
 		if (player.position.x < -100f) {
-			textUI.text = "TOO FAR LEFT!"; 
+			textUI.text = "You suck."; 
 		}
 		//if (player.position.x < 100f) {
-			//textUI.text = "TTOO FAR RIGHT!";
+		//textUI.text = "TTOO FAR RIGHT!";
 		//}
 		if ((goal.position - player.position).magnitude < 50f) {
-			textUI.text = "YOU'RE GETTING HELLA CLOSE";
-
+			textUI.text = "Hint = unicorns";
+			
 		}
 		if ((goal.position - player.position).magnitude < 10f) {
-			textUI.text = "press space idiot";
-			if ( Input.GetKeyDown (KeyCode.Space)) {
-				hasFoundTreasure = true;
-				//textUI.text = "you made it";
-			}
-
+			textUI.text = "think about it...";
 		}
-	if (hasFoundTreasure) {
-			textUI.text = "you win";
-		}
-	}
-
+}
 }
--- Assets/scripts/GameLogic3.cs
+++ Assets/scripts/GameLogic3.cs
@@ -2,43 +2,32 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class GameLogic : MonoBehaviour {
+
+public class GameLogic3 : MonoBehaviour {
 	public Text textUI;
 	public Transform player;
 	public Transform goal;
-	bool hasFoundTreasure = false;
 
 	// Use this for initialization
 	void Start () {
-
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//textBuffer += "\npress [W] to go forward";
-
 		if (player.position.x < -100f) {
-			textUI.text = "TOO FAR LEFT!"; 
+			textUI.text = "You suck."; 
 		}
 		//if (player.position.x < 100f) {
-			//textUI.text = "TTOO FAR RIGHT!";
+		//textUI.text = "TTOO FAR RIGHT!";
 		//}
 		if ((goal.position - player.position).magnitude < 50f) {
-			textUI.text = "YOU'RE GETTING HELLA CLOSE";
-
+			textUI.text = "Go to that weird glove";
+			
 		}
 		if ((goal.position - player.position).magnitude < 10f) {
-			textUI.text = "press space idiot";
-			if ( Input.GetKeyDown (KeyCode.Space)) {
-				hasFoundTreasure = true;
-				//textUI.text = "you made it";
-			}
-
+			textUI.text = "think about it...";
 		}
-	if (hasFoundTreasure) {
-			textUI.text = "you win";
-		}
+	
 	}
-
 }
