bb7eb6f3714f8294b75b47d5f373e5ce7215bc4f
Rameen Shafiee <rs4047@nyu.edu>
2015-09-22 00:24:20 -0400

    first draft update

---
+++ Assets/scripts/Cube1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube1 : MonoBehaviour {
+
+	public float moveSpeed = 500f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey (KeyCode.W) ) {
+			transform.position += new Vector3(0f, 0f, -50f) * Time.deltaTime;
+			transform.eulerAngles = new Vector3(0f, 0f, 0f);
+			//transform.rotation is BAD
+		} 
+		if ( Input.GetKey (KeyCode.S) ) {
+			transform.position += new Vector3(0f, 0f, 50f) * Time.deltaTime;
+			transform.eulerAngles = new Vector3(0f, 180f, 0f);
+		}
+		if ( Input.GetKey (KeyCode.A) ) {
+			transform.position += new Vector3(50f, 0f, 0f) * Time.deltaTime;
+			transform.eulerAngles = new Vector3(0f, -90f, 0f);
+		}
+		if ( Input.GetKey (KeyCode.D) ) {
+			transform.position += new Vector3(-50f, 0f, 0f) * Time.deltaTime;
+			transform.eulerAngles = new Vector3(0f, 90f, 0f);
+		}
+		if ( Input.GetKey (KeyCode.E) ) {
+			transform.position += new Vector3(0f, -1f, 0f);
+
+		}
+		if ( Input.GetKey (KeyCode.Q) ) {
+			transform.position += new Vector3(0f, 1f, 0f);
+		}
+
+		Camera.main.transform.position = transform.position + new Vector3(-7f, 15f, 25f);
+
+	
+	}
+}
---
+++ Assets/scripts/GameLogic.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+	public Text textUI;
+	public Transform player;
+	public Transform goal;
+	bool hasFoundTreasure = false;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (player.position.x < -100f) {
+			textUI.text = "Too far left!"; 
+		}
+		if ((goal.position - player.position).magnitude < 50f) {
+			textUI.text = "space blah win blah"; 
+		}
+		if ((goal.position - player.position).magnitude < 5f) {
+			textUI.text = "press space idiot";
+			if ( Input.GetKeyDown (KeyCode.Space)) {
+				hasFoundTreasure = true;
+				//textUI.text = "you made it";
+			}
+
+		}
+	if (hasFoundTreasure) {
+			textUI.text = "you win";
+		}
+	}
+
+}
